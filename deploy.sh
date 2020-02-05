2e4d9d33742f28de8729a57c839f656f4cf24053
trekkspace <trekkspace@gmail.com>
2020-02-05 11:19:10 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'dbd.trekkspace.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:trekkspace/dbd-skillcheck-simulator.git master:gh-pages
+
+cd -
